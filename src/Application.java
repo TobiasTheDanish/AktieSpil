ce4f3f14859bafcad1fb89a4e6a4581e74652cb1
TobiasTheDanish <t.hc199811@gmail.com>
2022-12-12 15:19:32 +0100

    FileIO ReadEquityData function added

--- src/Application.java
+++ src/Application.java
@@ -1,18 +1,28 @@
 import DataStructures.Stack.*;
 
+import java.util.ArrayList;
+
 public class Application {
     public final IStack<IMenu> menuStack = new Stack<>();
     public final IUI ui = new TextUI();
     public final IDataIO dataIO = new FileIO();
+    private ArrayList<IEquity> equities = new ArrayList<>();
     private User currentUser = null;
 
     public void run() {
+        equities = dataIO.readEquityData("src/Data/Stocks.csv");
+
         menuStack.push(new MainMenu());
         menuStack.push(new StartMenu());
 
         while (!menuStack.isEmpty()) {
             menuStack.peek().enter(this);
         }
+    }
+
+    public ArrayList<IEquity> getEquities()
+    {
+        return equities;
     }
 
     public User getCurrentUser() {
--- src/IDataIO.java
+++ src/IDataIO.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public interface IDataIO
 {
     ArrayList<String> readData(String path);
-    void writeNewUserData(String path, ArrayList<User> data);
+    ArrayList<IEquity> readEquityData(String path);
+    void writeNewUserData(String path, User user);
     void updateUserData(String path, User user);
     void writeGameData(String path, ArrayList<String> data);
 }
