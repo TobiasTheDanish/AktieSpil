fa7e1f38749937c384b0b20c535d7f33e85a04c6
TobiasBergg <cph-tb266@cphbusiness.dk>
2022-12-13 12:51:02 +0100

    12

--- src/TransactionManager.java
+++ src/TransactionManager.java
@@ -1,23 +1,18 @@
 public class TransactionManager {
 
+    public final IUI ui = new TextUI();
+
+    public void makeTransaction(IEquity equity, int amount, User user) {
+
+        float userBalance = user.getPortfolio().getBalance();
+        float stockPrice = equity.getPrice() * amount;
 
 
-    public void makeTransaction(IEquity equity, int amount, User user){
-
-      float userBalance = user.getPortfolio().getBalance();
-      float stockPrice = equity.getPrice() * amount;
-
-
-      if (userBalance >= stockPrice){
-          userBalance -= stockPrice;
-          //########################################
-          //Kan ikke få lov at bruge TextUI herinde!
-          //ToDo use TextUI.displayMessage("You have successfully bought " + equity.getName() + " .");
-          }
-      else {
-          //ToDo use TextUI.displayMessage("You don't have enough to buy " + equity.getName() + " .");
-          //########################################
-      }
+        if (userBalance >= stockPrice) {
+            userBalance -= stockPrice;
+            ui.displayMessage("You have successfully bought " + equity.getName() + " .");
+        } else {
+            ui.displayMessage("You don't have enough to buy " + equity.getName() + " .");
+        }
     }
-
 }
