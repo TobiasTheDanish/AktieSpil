97e019bed8ebb368b3f32bb9ba0960d3cbf5abe9
Bastian-cph <cph-bh213@cphbusiness.dk>
2022-12-13 15:18:08 +0100

    workspace

--- src/TransactionMenu.java
+++ src/TransactionMenu.java
@@ -7,11 +7,13 @@ String nameOfFunction;
     }
 
     private static void makeTransaction() {
-
+// evt en switch case her med en lille menu ligesom i mainmenu
+        // menuen kunne have mulighederne: 1: show list of available stocks press q to exit menu 
 
     }
     public void displayPrevTransactions () {
-
+    // evt en switch case her med en lille menu
+        // evt 1: display previous transactions press q to exit menu og en default som de andre
     }
     public void enter (Application application){
         TextUI textUI = application.ui.asTextUI();
